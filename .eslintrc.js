5e108abb34e0716cdfcc179a985b57cffed38f83
Demion-L <padovds@ukr.net>
2024-03-04 12:10:55 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["plugin:vue/essential"],
+  rules: {
+    semi: ["error", "always"], // Enforces semicolons
+    quotes: ["error", "double"],
+  },
+};
---
+++ src/components/MainCard.vue
@@ -0,0 +1,43 @@
+<template lang="">
+  <div class="main-container">
+    <div class="container-one">
+      <div class="profile">
+        <img src="" alt="" />
+        <h1>Dmytro Latysh</h1>
+        <p>Software Developer</p>
+      </div>
+      <div class="social-links">
+        <ul>
+          <li>
+            <img src="../assets/social media/github-94.png" alt="github logo" />
+            <a href=""> Github </a>
+          </li>
+          <li>
+            <img
+              src="../assets/social media/linkedin-logo-64.png"
+              alt="linkedin logo"
+            />
+            <a href=""> Linkedin </a>
+          </li>
+          <li>
+            <img
+              src="../assets/social media/facebook-circled-64.png"
+              alt="fb logo"
+            />
+            <a href=""> Facebook </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+img {
+  width: 20px;
+}
+</style>
