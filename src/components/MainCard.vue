ba24d3e7a7730cd5a5ae714f2679e7acb1eabbbc
Demion-L <padovds@ukr.net>
2024-03-05 17:59:10 +0200

    container-one styles refactored

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -9,7 +9,7 @@
       <div class="social-links">
         <ul>
           <li>
-            <a href="">
+            <a href="" data-replace="Github">
               <img
                 src="../assets/social media/github-94.png"
                 alt="github logo"
@@ -18,7 +18,7 @@
             </a>
           </li>
           <li>
-            <a href="">
+            <a href="" data-replace="Linkedin">
               <img
                 src="../assets/social media/linkedin-logo-64.png"
                 alt="linkedin logo"
@@ -27,7 +27,7 @@
             </a>
           </li>
           <li>
-            <a href="">
+            <a href="" data-replace="Facebook">
               <img
                 src="../assets/social media/facebook-circled-64.png"
                 alt="fb logo"
@@ -42,11 +42,31 @@
     <div class="container-two">
       <div class="about-me">
         <h1>About me</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-          id perferendis dolorum impedit deserunt dicta voluptatum odit veniam
-          minima. Est beatae voluptate maxime? Qui nihil quas quo, esse
-          accusamus debitis.
+        <p class="about-me__info">
+          Enthusiastic Full-Stack Developer. I'm a passionate developer with 2+
+          years of experience building web applications.
+          <br />
+          <br />
+          I'm currently expanding my skillset by learning advanced web UI
+          technologies. I'm particularly excited about the challenge of creating
+          user-friendly and visually appealing interfaces.
+          <br />
+          <br />
+          Recently, I completed a project using [mention specific technologies]
+          to build a [brief description of the project].
+          <br />
+          <br />
+          Previously, I worked as a Front-End Developer at WithSecure, where I
+          contributed to developing a complex cyber security portal using
+          Vue.js. Before that, I gained valuable experience as a Full-Stack
+          Developer at SoftServe, working on a large real estate project using
+          Node.js, TypeScript, and PostgreSQL.
+          <br />
+          <br />
+          I'm a strong team player with excellent communication and
+          collaboration skills. I'm eager to find a team where I can continue
+          learning, contribute to meaningful projects, and have fun
+          collaborating with colleagues.
         </p>
       </div>
       <div class="tech-section">
@@ -105,7 +125,7 @@ export default {};
 
 <style scoped lang="scss">
 img {
-  width: 30px;
+  width: 2rem;
 }
 
 ul,
@@ -123,11 +143,13 @@ a {
 
 .main-container {
   display: flex;
+  background: url("../assets/main\ bg/code.jpg");
 }
 
 .container {
   &-one {
-    background-color: rgba(240, 244, 186, 0.759);
+    background: rgba(229, 34, 34, 0.467),
+      url("../assets/main\ bg/coding-neon.jpg");
     width: 30%;
     display: flex;
     flex-direction: column;
@@ -141,29 +163,90 @@ a {
   }
 }
 
+.profile {
+  color: white;
+}
+
 .social-links {
   padding: 1.5%;
 
   ul {
     width: 80%;
-    border: 1px solid rgb(236, 62, 62);
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: flex-start;
-    background-color: rgba(202, 209, 67, 0.593);
+    background-color: rgba(245, 245, 7, 0.871);
     border-radius: 5px;
-    border: 1px solid rgba(255, 255, 255, 0.125);
+    border-top: 3px solid rgb(245, 245, 240);
+    border-left: 3px solid rgb(245, 245, 240);
+    border-bottom: 3px solid rgba(47, 59, 49, 0.404);
+    border-right: 3px solid rgba(47, 59, 49, 0.404);
   }
 
   li {
     padding: 1% 2.5%;
+    width: 55%;
 
     a {
       color: rgb(23, 24, 15);
       display: flex;
       align-items: center;
+      overflow: hidden;
+      position: relative;
+
+      &::before,
+      &::after {
+        content: "";
+        position: absolute;
+        width: 100%;
+        left: 0;
+      }
+      &::before {
+        background-color: #1b2bbb;
+        height: 2px;
+        bottom: 0;
+        transform-origin: 100% 50%;
+        transform: scaleX(0);
+        transition: transform 0.3s cubic-bezier(0.76, 0, 0.24, 1);
+      }
+      &::after {
+        content: attr(data-replace);
+        height: 100%;
+        top: 15%;
+        left: 10%;
+        transform-origin: 100% 50%;
+        transform: translate3d(200%, 0, 0);
+        transition: transform 0.3s cubic-bezier(0.76, 0, 0.24, 1);
+        color: #1b2bbb;
+        font-weight: bold;
+      }
+
+      &:hover::before {
+        transform-origin: 0% 50%;
+        transform: scaleX(1);
+      }
+      &:hover::after {
+        transform: translate3d(0, 0, 0);
+      }
+
+      & span {
+        display: inline-block;
+        transition: transform 0.3s cubic-bezier(0.76, 0, 0.24, 1);
+      }
+
+      &:hover span {
+        transform: translate3d(-200%, 0, 0);
+      }
     }
+  }
+}
+
+.about-me {
+  padding: 1rem;
+
+  &__info {
+    text-align: left;
   }
 }
 </style>
